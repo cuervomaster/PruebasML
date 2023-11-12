0d2d7ba62e0f6b2b9e383ae136e964f0ce393980
El cuervo <maestrocuervo11@gmail.com>
2023-11-12 07:51:36 -0500

    Bien

---
+++ Prueba1.py
@@ -0,0 +1,10 @@
+import math
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+
+
