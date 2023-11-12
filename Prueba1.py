3b1e1e321614ab3aba063fc3be86a0ab144b43ac
El cuervo <maestrocuervo11@gmail.com>
2023-11-12 09:17:20 -0500

    Bien

--- Prueba1.py
+++ Prueba1.py
@@ -6,5 +6,19 @@ from sklearn import linear_model
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
+num_records = 200
+
+url = \
+'http://wiki.stat.ucla.edu/socr/index.php/SOCR_Data_Dinov_020108_HeightsWeights'
+page = requests.get(url)
+height_weight_df = pd.read_html(url)[1][['Height(Inches)','Weight(Pounds)']]
+
+x = height_weight_df['Height(Inches)'].values.reshape(num_records, 1)
+y = height_weight_df['Weight(Pounds)'].values.reshape(num_records, 1)
+
+model = linear_model.LinearRegression().fit(x,y)
+
+print("ŷ =" + str(model.intercept_[0]) + " + " + str(model.coef_.T[0][0]) + " x₁")
+
 
 
