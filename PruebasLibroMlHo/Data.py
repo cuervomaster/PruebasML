49a45fb9fae54328c5feb1e38e2b6aa230095c9b
El cuervo <maestrocuervo11@gmail.com>
2023-11-19 18:20:38 -0500

    Bien

---
+++ PruebasLibroMlHo/Data.py
@@ -0,0 +1,28 @@
+import sys
+assert sys.version_info >= (3, 7)
+
+from packaging import version
+import sklearn
+assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
+
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+import matplotlib.pyplot as plt
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing = load_housing_data()
+
+housing.hist(bins=50,figsize=(12,8))
+plt.show()
